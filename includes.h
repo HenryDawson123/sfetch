c0d5dd0e9457e87e2ae47f1f3f3a1a2cdf4e63c3
Henry Dawson <henry@moid.lan>
2020-04-27 14:40:35 +0100

    Removed redundant/unused includes from the header file

--- includes.h
+++ includes.h
@@ -1,12 +1,11 @@
+/*using #pragma once here, idk whether i should use a different header guard instead but it is what it is*/
 #pragma once
-/*This is basically all of the headers for the main.cpp, they are in a seperate file for simplicity and easier navigation of main.cpp*/
+/*This is basically all of the includes/headers for the main.cpp, they are in a seperate file for simplicity and easier navigation of main.cpp*/
 
 #include <fstream>
 #include <sstream>
-#include <algorithm>
 #include <string>
-#include <stdlib.h>
-#include <unistd.h>
+#include <algorithm>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
 #include <chrono>
